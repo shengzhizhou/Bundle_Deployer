35849a1c0dfc126931d6d89b5a8dbd941d371935
shengzhizhou <40223156+shengzhizhou@users.noreply.github.com>
2019-08-19 15:33:14 -0400

    first_draft

--- sample/bundle_deployer.py
+++ sample/bundle_deployer.py
@@ -5,12 +5,53 @@ import json
 import os
 import argparse
 
+TYPE_COUNT = {
+    "R": 0,
+    "D": 0,
+    "A": 0,
+    "M": 0,
+    "C": 0,
+}
+
+TYPE_NAME = {
+    "R": "Renamed Files",
+    "D": "Deleted Files",
+    "A": "Added Files",
+    "M": "Modified Files",
+    "C": "Copied Files"
+}
 
 def deploy_bundle(args):
-    f=open(os.path.join(args.rootpath,"build","manifest.txt"),'r')
-    text=f.read()
-    data=json.loads(text)
-    print(data['info'][0].get('path'))
+    f = open(os.path.join(args.rootpath, "build", "manifest.txt"), 'r')
+    text = f.read()
+    data = json.loads(text)
+    for file in data['info']:
+        if file.get('type')[0] == 'D':
+            delete_file(args, file.get('to_path'))
+        if file.get('type')[0] == 'R':
+            rename_file(args , file.get('from_path'),file.get('to_path'))
+        if file.get('type')[0] == 'C':
+            add_file(args, file.get('to_path'))
+        else:
+            add_file(args, file.get('to_path'))
+        count(file.get('type'))
+
+
+def delete_file(args,path):
+    os.remove(os.path.join(args.destpath, path))
+
+def count(type):
+    change_type = type[0]
+    TYPE_COUNT[change_type] = TYPE_COUNT[change_type] + 1
+
+def rename_file(args ,from_path, to_path):
+    delete_file(args,from_path)
+    add_file(args,to_path)
+
+
+def add_file(args,path):
+    os.makedirs(os.path.join(args.destpath, os.path.dirname(path)), exist_ok=True)
+    shutil.copy(os.path.join(args.rootpath, "src", path), os.path.join(args.destpath, path))
 
 
 
@@ -20,6 +61,8 @@ def main():
     parser.add_argument('-rp', '--rootpath', required=True, help='Input Extraction Folder Root Path')
     args = parser.parse_args()
     deploy_bundle(args)
+    for value in TYPE_COUNT:
+        print(TYPE_NAME[value], " : ", TYPE_COUNT[value])
     print("Success")
 
 
--- sample/bundle_packager.py
+++ sample/bundle_packager.py
@@ -5,22 +5,45 @@ from zipfile import ZipFile
 import os
 import argparse
 
+TYPE_COUNT = {
+    "R": 0,
+    "D": 0,
+    "A": 0,
+    "M": 0,
+    "C": 0,
+}
+
+TYPE_NAME = {
+    "R": "Renamed Files",
+    "D": "Deleted Files",
+    "A": "Added Files",
+    "M": "Modified Files",
+    "C": "Copied Files"
+}
+
 
 def create_bundle_zip(args):
     create_bundle_dir(args)
     create_manifest_file(args)
     with ZipFile(os.path.join(args.zippath, args.srcbranch + '.zip'), 'w') as zipfile:
         for stats in get_diff_info(args.srcpath, args.srcbranch, args.destbranch):
+            count(stats.get('type'))
             if stats.get("type") != 'D':
-                add_files(stats.get("path"), args)
-                zipfile.write(os.path.join(args.zippath, "src", stats.get('path')),  # zip file from directory
-                              os.path.join("src", stats.get('path')))  # to directory
-        zipfile.write(os.path.join(args.zippath, "build", "manifest.txt"), os.path.join("build", "manifest.txt"))
+                add_files(stats.get("to_path"), args)
+                zipfile.write(os.path.join(args.zippath, "bundle-temp", "src", stats.get('to_path')),
+                              os.path.join("src", stats.get('to_path')))  # to directory
+        zipfile.write(os.path.join(args.zippath, "bundle-temp", "build", "manifest.txt"),
+                      os.path.join("build", "manifest.txt"))
     delete_bundle_dir(args)
 
 
+def count(type):
+    change_type = type[0]
+    TYPE_COUNT[change_type] = TYPE_COUNT[change_type] + 1
+
+
 def create_manifest_file(args):
-    f = open(os.path.join(args.zippath, "build", "manifest.txt"), "w+")
+    f = open(os.path.join(args.zippath, "bundle-temp", "build", "manifest.txt"), "w+")
     f.write(manifest_info_text(args))
     f.close()
 
@@ -28,7 +51,7 @@ def create_manifest_file(args):
 def manifest_info_text(args):
     txt = "{\'name\':\'manifest\',\'info\':["
     for stats in get_diff_info(args.srcpath, args.srcbranch, args.destbranch):
-        print(stats)
+        # print(stats)
         txt += str(stats) + ","
     txt = txt[:-1]
     txt += "]}"
@@ -37,18 +60,17 @@ def manifest_info_text(args):
 
 
 def create_bundle_dir(args):
-    os.mkdir(os.path.join(str(args.zippath), "src"))
-    os.mkdir(os.path.join(str(args.zippath), "build"))
+    os.makedirs(os.path.join(str(args.zippath), "bundle-temp", "src"))
+    os.makedirs(os.path.join(str(args.zippath), "bundle-temp", "build"))
 
 
 def delete_bundle_dir(args):
-    shutil.rmtree(os.path.join(str(args.zippath), "src"))
-    shutil.rmtree(os.path.join(str(args.zippath), "build"))
+    shutil.rmtree(os.path.join(str(args.zippath), "bundle-temp"))
 
 
 def add_files(path, args):
-    os.makedirs(os.path.join(args.zippath, "src", os.path.dirname(path)), exist_ok=True)
-    shutil.copy(os.path.join(args.srcpath, path), os.path.join(args.zippath, "src", path))
+    os.makedirs(os.path.join(args.zippath, "bundle-temp", "src", os.path.dirname(path)), exist_ok=True)
+    shutil.copy(os.path.join(args.srcpath, path), os.path.join(args.zippath, "bundle-temp", "src", path))
 
 
 def get_diff_info(path, src_branch, dest_branch):
@@ -63,8 +85,9 @@ def get_diff_info(path, src_branch, dest_branch):
 
     for diff in dest_commit.diff(source_commit):
         stats = {
-            'path': diff.a_path,
-            'filename': diff.a_path,
+            'from_path': diff.a_path,
+            'to_path': diff.b_path,
+            'filename': os.path.basename(diff.a_path),
             'type': diff.change_type,
         }
         yield stats
@@ -72,14 +95,17 @@ def get_diff_info(path, src_branch, dest_branch):
 
 def main():
     parser = argparse.ArgumentParser(description='Create Bundle')
-    parser.add_argument('-sp', '--srcpath', required=True, help='Input src Repo Local Path')
+    parser.add_argument('-sp', '--srcpath', required=True, help='Input source Repo Local Path')
     parser.add_argument('-zp', '--zippath', required=True, help='Input root Path for bundle zip file')
-    parser.add_argument('-sb', '--srcbranch', required=True, help='Input src branch')
+    parser.add_argument('-sb', '--srcbranch', required=True, help='Input source branch')
     parser.add_argument('-db', '--destbranch', required=True, help='Input destination branch')
 
     args = parser.parse_args()
     print("Start creating bundle zip file\n")
     create_bundle_zip(args)
+    for value in TYPE_COUNT:
+        print(TYPE_NAME[value], " : ", TYPE_COUNT[value])
+
     print("\nSuccess")
 
 
